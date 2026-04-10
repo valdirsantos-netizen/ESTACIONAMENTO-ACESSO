0234d74e37a543a76433d1f4c7e1d9576789f7cf
valdirsantos-netizen <valdir.santos@mercadolivre.com>
2026-04-10 11:03:28 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" suppressHydrationWarning>
       <body>
         {children}
       </body>
