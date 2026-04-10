cc3e0822805f43cfc03443de659810779c244885
valdirsantos-netizen <valdir.santos@mercadolivre.com>
2026-04-10 10:03:52 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body style={{ margin: 0, backgroundColor: '#f0f2f5', fontFamily: 'sans-serif' }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
